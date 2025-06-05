2b4452577eff4076da4837a8deb49bee23a47e62
sbsmatthie <39104487+sbsmatthie@users.noreply.github.com>
2025-06-05 18:21:23 +0200

    Update wallet-badge.tsx

--- src/components/layout/header/wallets/wallet-badge.tsx
+++ src/components/layout/header/wallets/wallet-badge.tsx
@@ -3,17 +3,12 @@ import Badge from '@/components/shared_ui/badge';
 import { localize } from '@deriv-com/translations';
 
 type TWalletBadge = {
-    is_demo: boolean;
+    is_demo: false;
     label?: string;
 };
 
 const WalletBadge = ({ is_demo, label }: TWalletBadge) => {
-    const active_loginid = localStorage.getItem('active_loginid');
-    const real_force_logins = ['CR1234567']; // list of loginids that should always show real badge
-    const forceRealBadge = true; //real_force_logins.includes(active_loginid ?? '');
-    const showAsDemo = is_demo && !forceRealBadge;
-
-    return showAsDemo ? (
+    return is_demo ? (
         <Badge type='contained' background_color='blue' label={localize('Demo')} custom_color='colored-background' />
     ) : (
         <Badge type='bordered' label={label?.toUpperCase() ?? ''} />

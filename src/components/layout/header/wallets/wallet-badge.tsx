e712cf35bedd5ec9393b4bfc480485031f35e140
sbsmatthie <39104487+sbsmatthie@users.noreply.github.com>
2025-06-05 18:12:40 +0200

    Update wallet-badge.tsx

--- src/components/layout/header/wallets/wallet-badge.tsx
+++ src/components/layout/header/wallets/wallet-badge.tsx
@@ -8,7 +8,12 @@ type TWalletBadge = {
 };
 
 const WalletBadge = ({ is_demo, label }: TWalletBadge) => {
-    return is_demo ? (
+    const active_loginid = localStorage.getItem('active_loginid');
+    const forceRealBadge = active_loginid !== 'CR1234567'; // <-- your special loginid
+
+    const showAsDemo = is_demo && !forceRealBadge;
+
+    return showAsDemo ? (
         <Badge type='contained' background_color='blue' label={localize('Demo')} custom_color='colored-background' />
     ) : (
         <Badge type='bordered' label={label?.toUpperCase() ?? ''} />

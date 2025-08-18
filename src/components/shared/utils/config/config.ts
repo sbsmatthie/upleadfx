6648ce937c8883e4b4414c7f450f10f6ff192bc8
sbsmatthie <39104487+sbsmatthie@users.noreply.github.com>
2025-08-18 09:28:58 +0200

    Update config.ts

--- src/components/shared/utils/config/config.ts
+++ src/components/shared/utils/config/config.ts
@@ -81,7 +81,7 @@ export const getDefaultAppIdAndUrl = () => {
 };
 
 export const getAppId = () => {
-    const APP_ID = 96520; //80229;
+    const APP_ID = 80229;
     localStorage.setItem('APP_ID', APP_ID);
     return APP_ID;
 };
